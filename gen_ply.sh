0350e0867321a52d87fed62d8d82209e278775a9
2bit <i@2bit.jp>
2023-09-05 00:12:44 +0900

    upadte

--- gen_ply.sh
+++ gen_ply.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-models=("easy-khair-180-gpc0.8-trans10-025000.pkl"\
-  "ablation-trigridD-1-025000.pkl")
+models=("easy-khair-180-gpc0.8-trans10-025000.pkl")
+# models=("ablation-trigridD-1-025000.pkl")
 
 in="models"
 out="pti_out"
@@ -12,12 +12,9 @@ NUM_STEPS_PTI=300
 for model in ${models[@]}
 
 do
-
-    for i in 0 1
-
+    for i in 0
     do 
         # perform the pti and save w
         python projector_withseg.py --shapes=True --num-steps=${NUM_STEPS} --num-steps-pti=${NUM_STEPS_PTI} --outdir=${out} --target_img=dataset/testdata_img --network ${in}/${model} --idx ${i}
     done
-
 done
